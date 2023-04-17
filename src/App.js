a0ff3b4747c35576110c6c2df9f21e63770b18e8
Collins Adaigho <donsmatt44@gmail.com>
2023-04-17 16:52:22 +0100

    Template developed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <div className="container">
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </div>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam nostrum numquam magnam modi ipsam temporibus repudiandae molestias autem facilis! Tempore illum aperiam aliquam nostrum ea inventore repellendus amet nam. Nesciunt neque impedit inventore assumenda, illo fugit quod sunt repudiandae odit voluptates, perferendis sit nam unde consectetur fuga ab eos voluptatibus voluptate! Ex minus facere velit. Culpa, obcaecati vitae ab cum consequuntur quibusdam dolore dignissimos. Officia fugiat accusantium sit quod libero. Soluta debitis, nisi eum porro obcaecati doloribus laborum nulla suscipit fugit itaque deserunt reiciendis ratione eveniet dolore nihil enim consequuntur necessitatibus? Quos adipisci repudiandae dolore, recusandae totam aut voluptate.</div>;
+};
+
+export default Home;
