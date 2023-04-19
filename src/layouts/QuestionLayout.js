1ba93495df57f3e2970c404425440d4e1abdc842
Collins Adaigho <donsmatt44@gmail.com>
2023-04-19 07:25:05 +0100

    The arrows don tire me

--- src/layouts/QuestionLayout.js
+++ src/layouts/QuestionLayout.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Outlet, useLoaderData } from "react-router";
-import { Link } from "react-router-dom";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { Outlet } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const QuestionLayout = () => {
-  const questions = useLoaderData();
+  // const questions = useLoaderData();
 
   return (
     <div>
       <div className="questions">
-        {questions.map((question) => (
-          <p key={question.id} className="question-number">
-            {<Link to="/">{question.id}</Link>}
-          </p>
-        ))}
+        <NavLink to="4">
+          <span>{<BsArrowLeftCircleFill className="arrows" />}</span>
+        </NavLink>
+        <NavLink to="2">
+          <span>{<BsArrowRightCircleFill className="arrows" />}</span>
+        </NavLink>
       </div>
       <Outlet />
     </div>
