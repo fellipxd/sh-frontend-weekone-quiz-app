0f4b793fa2d2593ac0e7fd6fd5d6957f3b465eff
Collins Adaigho <donsmatt44@gmail.com>
2023-04-20 15:24:11 +0100

    merge conflict

--- src/layouts/QuestionLayout.js
+++ src/layouts/QuestionLayout.js
@@ -12,30 +12,17 @@ const QuestionLayout = () => {
   const [next, setNext] = useState(id);
 
   const handlePrevClick = () => {
-    const newPrev = +prev - 1
+    const newPrev = +prev - 1;
     setPrev(newPrev.toString());
   };
 
   const handleNextClick = () => {
-    const newNext = +next + 1
+    const newNext = +next + 1;
     setNext(newNext.toString());
   };
 
   return (
     <div>
-<<<<<<< HEAD
-      <div className="questions">
-        <NavLink onClick={handlePrevClick} to={prev}>
-=======
-      {/* <div className="questions">
-        <NavLink to="4">
->>>>>>> 73fb8bb4f9e2e5d5e639774b9be665ef30befffc
-          <span>{<BsArrowLeftCircleFill className="arrows" />}</span>
-        </NavLink>
-        <NavLink onClick={handleNextClick} to={next}>
-          <span>{<BsArrowRightCircleFill className="arrows" />}</span>
-        </NavLink>
-      </div> */}
       <Outlet />
     </div>
   );
